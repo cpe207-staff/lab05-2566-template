79b6f08a2a8848c197efba7e2dccc66bba651922
D. Potikanond <potikanond@gmail.com>
2024-07-03 21:06:59 +0700

    Update and rename q2.js to q2.ts
    add some comments

--- q2.ts
+++ q2.ts
@@ -1,7 +1,12 @@
+// define interface for Student object
+/* Your code here */
+
+// assign interface/type to the function definition properly
 function findTopNames(students) {
   /* Your code here */
 }
 
+// assign interface/type to the student1 object properly
 const students1 = [
   { name: "john", score: 10 },
   { name: "jane", score: 9 },
